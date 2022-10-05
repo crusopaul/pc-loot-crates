9974027d263b9f905138e36a0b4d517286a0815a
crusopaul <crusopaul@gmail.com>
2022-10-05 16:25:08 -0400

    Correction casing in config.lua

--- config.lua
+++ config.lua
@@ -4,8 +4,8 @@ Config = {
             MinDrops = 1,
             MaxDrops = 1,
             LootTable = {
-                1 = { item = 'water_bottle', chances = '1' },
-                2 = { item = 'sandwich', chances = '2' },
+                1 = { Item = 'water_bottle', Chances = '1' },
+                2 = { Item = 'sandwich', Chances = '2' },
             }
         },
     }
